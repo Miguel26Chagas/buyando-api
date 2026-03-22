42d057dfbb44059d0267316fdb525ef0fe6d9e28
Miguel26Chagas <miguel26chagas@gmail.com>
2026-03-22 16:51:24 +0100

    Alter in Teste 1

--- testes.py
+++ testes.py
@@ -1,8 +1,11 @@
-from fastapi import BackgroundTasks, FastAPI
+while (True): 
+    entrar = input(f'digite zero: ')
+    if entrar == '0':
+        print('O Sistema Fodeu!!!')
+        break
+    elif entrar == '1': 
+        print('Tudo ok')
+    else:
+        print(f'um {entrar}, Fudeu o sistema')
+        break
 
-app = FastAPI()
-
-@app.post("/send-notification/{email}")
-async def send_notification(email: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(write_notification, email, message="some notification")
-    return {"message": "Notification sent in the background"}
